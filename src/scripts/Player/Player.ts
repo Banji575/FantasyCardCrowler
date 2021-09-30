146e44ddaa885710478099a43125201331f23c8a
Sergey <banjamin@yandex.ru>
2021-09-30 18:01:19 +0300

    player sync

--- src/scripts/Player/Player.ts
+++ src/scripts/Player/Player.ts
@@ -1,18 +1,27 @@
 export default class Player extends Phaser.GameObjects.Sprite{
+    player
     constructor(public scene: Phaser.Scene, public x:number, public y:number,texture: string, frame:number){
         super(scene, x, y, texture)
         this.init()
-        return this
+        return this.player
     }
 
     init(){
         this.scene.add.existing(this).setOrigin(1).setScale(1)
-        this.scene.physics.add.existing(this)
         this.animCreate()
         this.play('idleAnim')
-        
-        
-        
+        this.player = this.createPlayerCollider()
+    }
+
+     colliderSync(){
+        this.x = this.player.x
+        this.y = this.player.y
+    }
+
+    private createPlayerCollider(){
+       const rect = this.scene.add.rectangle(300,300, 50,50)
+        this.scene.physics.add.existing(rect)
+        return rect
     }
 
     private animCreate(){
--- src/scripts/scenes/Main.ts
+++ src/scripts/scenes/Main.ts
@@ -2,6 +2,7 @@ import Level from "../level/Level"
 import Player from "../Player/Player"
 
 export default class Main extends Phaser.Scene{
+    player
     constructor(){
         super('main')
         
@@ -9,8 +10,12 @@ export default class Main extends Phaser.Scene{
     create(){
         const {width, height}= this.game.config
         const level = new Level(this)
-        const player = new Player(this, 350 ,150,'playerIdle',1)
+        this.player = new Player(this, 350 ,150,'playerIdle',1)
 
-        this.physics.add.collider(level.platform, player, ()=>{})
+        this.physics.add.collider(level.platform, this.player, ()=>{})
+    }
+
+    update(){
+        this.player.colliderSync()
     }
 }
