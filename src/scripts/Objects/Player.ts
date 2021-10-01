264a19444ce686254856f6d5b2c9d1256653b54f
Sergey <banjamin@yandex.ru>
2021-10-01 18:02:25 +0300

    new commit

--- src/scripts/Objects/Player.ts
+++ src/scripts/Objects/Player.ts
@@ -3,13 +3,16 @@ import Character from "./Character"
 
 export default class Player extends Character{
     player
+    isRun
     constructor(public scene: Phaser.Scene, public x:number, public y:number,texture: string, frame:number){
         super(scene, x, y, texture, frame)
+        this.isRun = false
         this.initial()
     }
 
     initial(){
         this.animCreate()
+        this.animRunCreate()
         //this.scene.physics.add.existing(this)
         this.play('idleAnim')
         
@@ -24,12 +27,25 @@ export default class Player extends Character{
             repeat:-1
         })
     }
+    private animRunCreate(){
+        this.anims.create({
+            key:'playerRun',
+            frames: this.anims.generateFrameNames('playerRun'),
+            frameRate:8,
+            repeat:-1
+        })
+    }
 
     move(dir:string | number){
-            const x = keyboardDirection[dir].x
-
+            const {x,y} = keyboardDirection[dir]
+            this.x += x
+            this.y += y
+            if((x!==0|| y!==0) && !this.isRun){
+                this.play('playerRun')
+                this.isRun = true
+            }
        
-        this.y -= 1
+       
     }
 
 }
--- src/scripts/scenes/Preload.ts
+++ src/scripts/scenes/Preload.ts
@@ -8,6 +8,8 @@ export class Preload extends Phaser.Scene{
         this.load.image('tileMap1','assets/maps/pixel_platform_01_tileset_final.png')
         this.load.tilemapTiledJSON('level1','assets/maps/maps.json')
         this.load.spritesheet('playerIdle', 'assets/player/Idle2.png',{frameWidth:135, frameHeight:135})
+        this.load.spritesheet('playerRun', 'assets/player/Run.png',{frameWidth:135, frameHeight:135})
+
 
         this.load.tilemapTiledJSON('mainTownMap', 'assets/maps/topDownMap/mainTown2.json')
         this.load.image('mainWallTileset', 'assets/maps/topDownMap/TX Tileset Wall.png')
