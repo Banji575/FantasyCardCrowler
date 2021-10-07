ce1c087ca804079b597646370fe116321f0e4bc8
Sergey <Banjamin@yandex.ru>
2021-10-07 23:08:40 +0300

    finished magic effect

--- src/scripts/scenes/Preload.ts
+++ src/scripts/scenes/Preload.ts
@@ -1,14 +1,14 @@
 import Phaser from "phaser";
 
-export class Preload extends Phaser.Scene{
-    constructor(){
+export class Preload extends Phaser.Scene {
+    constructor() {
         super('preload')
     }
-    preload(){
-        this.load.image('tileMap1','assets/maps/pixel_platform_01_tileset_final.png')
-        this.load.tilemapTiledJSON('level1','assets/maps/maps.json')
-        this.load.spritesheet('playerIdle', 'assets/player/Idle2.png',{frameWidth:135, frameHeight:135})
-        this.load.spritesheet('playerRun', 'assets/player/Run.png',{frameWidth:135, frameHeight:135})
+    preload() {
+        this.load.image('tileMap1', 'assets/maps/pixel_platform_01_tileset_final.png')
+        this.load.tilemapTiledJSON('level1', 'assets/maps/maps.json')
+        this.load.spritesheet('playerIdle', 'assets/player/Idle2.png', { frameWidth: 135, frameHeight: 135 })
+        this.load.spritesheet('playerRun', 'assets/player/Run.png', { frameWidth: 135, frameHeight: 135 })
 
 
         this.load.tilemapTiledJSON('mainTownMap', 'assets/maps/topDownMap/mainTown2.json')
@@ -16,8 +16,8 @@ export class Preload extends Phaser.Scene{
         this.load.image('mainGrassTileset', 'assets/maps/topDownMap/TX Tileset Grass.png')
         this.load.image('mainPlacesTileset', 'assets/maps/topDownMap/TX Tileset Stone Ground.png')
         this.load.image('mainItemsTileset', 'assets/maps/topDownMap/TX Props.png')
-        this.load.image('mainTreeTileset','assets/maps/topDownMap/TX Plant.png' )
-        this.load.image('mainStraisTileset','assets/maps/topDownMap/TX Struct.png')
+        this.load.image('mainTreeTileset', 'assets/maps/topDownMap/TX Plant.png')
+        this.load.image('mainStraisTileset', 'assets/maps/topDownMap/TX Struct.png')
 
         //interface element
         this.load.image('cardPlace', 'assets/img/inteface/cardPlace.png')
@@ -32,19 +32,20 @@ export class Preload extends Phaser.Scene{
         this.load.image('MediumFireball', 'assets/img/cardArt/mediumFireball.jpg')
 
         //enemy
-        this.load.spritesheet('assasinIdle', 'assets/BattleImg/Enemyes/assasin/Idle.png',{frameWidth:150, frameHeight:128})
+        this.load.spritesheet('assasinIdle', 'assets/BattleImg/Enemyes/assasin/Idle.png', { frameWidth: 150, frameHeight: 128 })
 
         //magic
-        this.load.spritesheet('Medium Fireball_magic', 'assets/img/magic/FireBall_3_64x64.png', {frameWidth:64, frameHeight:64})
+        this.load.spritesheet('Medium Fireball_magic', 'assets/img/magic/FireBall_3_64x64.png', { frameWidth: 64, frameHeight: 64 })
+        this.load.spritesheet('Medium Fireball_effect', 'assets/img/magic/FireBurst_64x64.png', { frameWidth: 64, frameHeight: 64 })
 
     }
 
-    create(){
+    create() {
         this.scene.start('main')
     }
 
 
-    initSystemClass(){
-        
+    initSystemClass() {
+
     }
 }
